2cac98323bc9559b22fd573bfa174ea50a691614
Jody Mazinyi <jodyclelland8@gmail.com>
2021-03-26 11:33:27 +1100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Jody Clelland on 26/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
