744c4007c58c7c016b74d7501c216d721466f3e4
Jody Mazinyi <jodyclelland8@gmail.com>
2021-03-30 11:25:39 +1100

    added in second animation to current image

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,22 +10,29 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var change = false
+    @State private var changetwo = false
 
     var body: some View {
 
         VStack{
             Button("Twist The Avo") {
                 self.change.toggle()
-            }
-            .padding(.bottom, 100)
+            }.padding(.bottom, 200)
            
             
              Image("Avo")
             .resizable()
+            .frame(width: 150, height: 150)
             .aspectRatio(contentMode: .fill)
-                .frame(width: 300, height: 300)
             .rotationEffect(Angle.degrees(change ? 220 : 0))
             .animation(.easeOut)
+                .scaleEffect(changetwo ? 2 : 1)
+                .animation(.easeInOut)
+            
+            
+            Button("Grow The Avo") {
+                self.changetwo.toggle()
+            }.padding(.top, 200)
 
         }
        
